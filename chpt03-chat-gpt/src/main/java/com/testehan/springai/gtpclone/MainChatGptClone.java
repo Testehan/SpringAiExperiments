d8690add032517bb9b7a5a708106223e369b20ad
DanTestehan <tdan89@yahoo.com>
2024-07-07 20:20:20 +0100

    chpt 3 - working on a gpt chat clone

---
+++ chpt03-chat-gpt/src/main/java/com/testehan/springai/gtpclone/MainChatGptClone.java
@@ -0,0 +1,11 @@
+package com.testehan.springai.gtpclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainChatGptClone {
+    public static void main(String[] args) {
+        SpringApplication.run(MainChatGptClone.class, args);
+    }
+}
