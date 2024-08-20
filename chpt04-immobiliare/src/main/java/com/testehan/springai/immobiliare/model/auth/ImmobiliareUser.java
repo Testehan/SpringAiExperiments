d90f2a166b84c3def2b66c6c11d6dc3c06f4a3d8
DanTestehan <tdan89@yahoo.com>
2024-08-20 20:20:20 +0100

    chpt 4 - simplified User class by using lombok

--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/model/auth/ImmobiliareUser.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/model/auth/ImmobiliareUser.java
@@ -1,13 +1,20 @@
 package com.testehan.springai.immobiliare.model.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class ImmobiliareUser {
 
-//    @BsonProperty("_id")
-    ObjectId id;
+    private ObjectId id;
 
     private String email;
 
@@ -19,51 +26,4 @@ public class ImmobiliareUser {
 
     private List<String> favourites;
 
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public AuthenticationType getAuthenticationType() {
-        return authenticationType;
-    }
-
-    public void setAuthenticationType(AuthenticationType authenticationType) {
-        this.authenticationType = authenticationType;
-    }
-
-    public List<String> getFavourites() {
-        return favourites;
-    }
-
-    public void setFavourites(List<String> favourites) {
-        this.favourites = favourites;
-    }
 }
