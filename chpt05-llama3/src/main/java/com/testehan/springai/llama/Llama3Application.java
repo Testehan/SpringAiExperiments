5d09fd4b2814010a2f199baf6caec2104de00874
DanTestehan <tdan89@yahoo.com>
2024-08-31 20:20:20 +0100

    chpt05 - function calling - properties for weatherapi.com defined

--- chpt05-llama3/src/main/java/com/testehan/springai/llama/Llama3Application.java
+++ chpt05-llama3/src/main/java/com/testehan/springai/llama/Llama3Application.java
@@ -1,8 +1,13 @@
 package com.testehan.springai.llama;
 
+import com.testehan.springai.llama.functions.WeatherConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+// below annotation is needed so that the properties defined in WeatherConfigProperties are
+// configured and injectable
+@EnableConfigurationProperties(WeatherConfigProperties.class)
 @SpringBootApplication
 public class Llama3Application {
 
