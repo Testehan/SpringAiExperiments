842113aae0f502f4ac5cba6975554851e8cfb5ca
DanTestehan <tdan89@yahoo.com>
2024-08-31 20:20:20 +0100

    chpt05 - main class and config file

---
+++ chpt05-llama3/src/main/java/com/testehan/springai/llama/Llama3Application.java
@@ -0,0 +1,13 @@
+package com.testehan.springai.llama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Llama3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Llama3Application.class, args);
+    }
+
+}
