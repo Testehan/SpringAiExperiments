4a16d45d4fe5b8d36b4eee7cb0a7d28772580da7
DanTestehan <tdan89@yahoo.com>
2024-09-01 20:20:20 +0100

    chpt04 - a user message is automatically added to the model and afterwards the request to get a response is made to make things smoother in the UI

--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/controller/ChatController.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/controller/ChatController.java
@@ -4,20 +4,17 @@ import com.testehan.springai.immobiliare.model.ResultsResponse;
 import com.testehan.springai.immobiliare.service.ApiService;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxResponse;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
-import jakarta.servlet.http.HttpSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @CrossOrigin
 public class ChatController {
 
-    private static final Logger log = LoggerFactory.getLogger(ChatController.class);
     private final ApiService apiService;
 
     public ChatController(ApiService apiService) {
@@ -25,13 +22,12 @@ public class ChatController {
     }
 
     @HxRequest
-    @PostMapping("/chat")
-    public HtmxResponse generate(@RequestParam String message, HttpSession session, Model model) {
+    @PostMapping("/respond")
+    public HtmxResponse respond(@RequestParam String message, Model model) {
 
         ResultsResponse response = apiService.getChatResponse(message);
 
         if (response.containsApartments()){
-            model.addAttribute("message",message);
             model.addAttribute("response",response.message());
             model.addAttribute("apartments",response.apartments());
 
@@ -39,14 +35,22 @@ public class ChatController {
                     .view("response :: responseFragmentWithApartments")
                     .build();
         } else {
-
-            model.addAttribute("message",message);
             model.addAttribute("response",response.message());
 
             return HtmxResponse.builder()
                     .view("response :: responseFragment")
                     .build();
         }
+    }
+
+    // Add a user message to model but don't ask to respond to it. This enables us to update the UI quickly.
+    @HxRequest
+    @PutMapping("/message")
+    public HtmxResponse addUserMessage(@RequestParam String message, Model model) {
+        model.addAttribute("message",message);
+        return HtmxResponse.builder()
+                .view("response :: responseFragment")
+                .build();
     }
 
 }
