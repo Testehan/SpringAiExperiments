d4b574964ab1500a97cbc102492a0fb06ff6ed40
DanTestehan <tdan89@yahoo.com>
2024-10-21 20:20:20 +0100

    chpt04-immobiliare - refactoring and work on generating metadata for apartment images

--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/controller/ApartmentController.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/controller/ApartmentController.java
@@ -1,25 +1,12 @@
 package com.testehan.springai.immobiliare.controller;
 
-import com.testehan.springai.immobiliare.constants.AmazonS3Constants;
 import com.testehan.springai.immobiliare.model.Apartment;
 import com.testehan.springai.immobiliare.security.UserService;
 import com.testehan.springai.immobiliare.service.ApartmentService;
-import com.testehan.springai.immobiliare.service.OpenAiService;
-import com.testehan.springai.immobiliare.util.AmazonS3Util;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.SystemMessage;
-import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.model.Media;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MimeTypeUtils;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,29 +14,20 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/api/apartments")
 public class ApartmentController {
 
-    private final OpenAiService openAiService;
-
     private final ApartmentService apartmentService;
     private final UserService userService;
-    private final ChatModel chatModel ;
 
-    public ApartmentController(OpenAiService openAiService, ApartmentService apartmentService, UserService userService,
+
+    public ApartmentController(ApartmentService apartmentService, UserService userService,
                                ChatModel chatModel)
     {
-        this.openAiService = openAiService;
         this.apartmentService = apartmentService;
         this.userService = userService;
-        this.chatModel = chatModel;
     }
 
     @PostMapping("/save")
@@ -60,60 +38,15 @@ public class ApartmentController {
 
         var user = userService.getImmobiliareUserByEmail(userEmail);
         if (user.getMaxNumberOfListedApartments() > 0){
-            LocalDateTime now = LocalDateTime.now();
-            DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            String formattedDateCustom = now.format(customFormatter);
-
-            apartment.setCreationDateTime(formattedDateCustom);
-            apartment.setLastUpdateDateTime(formattedDateCustom);
-            var apartmentInfoToEmbed = apartment.getApartmentInfoToEmbedd();
-
-            var mono = openAiService.createEmbedding(apartmentInfoToEmbed);
-            List<Double> embeddings = mono.block();
-            System.out.println(embeddings.stream().map( d -> d.toString()).collect(Collectors.joining(" ")));
-            apartment.setPlot_embedding(embeddings);
-
-            apartmentService.saveApartment(apartment);
-            saveUploadedImages(apartment, apartmentImages);
-            generateMetadata(apartmentImages);
-            apartmentService.saveApartment(apartment);
-
-            user.setMaxNumberOfListedApartments(user.getMaxNumberOfListedApartments() - 1);
-            userService.updateUser(user);
+            apartmentService.saveApartmentAndImages(apartment, apartmentImages, user);
 
             return "redirect:/";
         } else {
             redirectAttributes.addFlashAttribute("errorMessage","ERROR: You have reached the maximum number of listed apartments!");
             return "redirect:/error";
         }
-
-
     }
 
-    private void saveUploadedImages(Apartment apartment, MultipartFile[] apartmentImages) throws IOException {
-        if (apartmentImages.length>0) {
-            var uploadDir = "apartment-images/" + apartment.getId();
-            for (MultipartFile extraImage : apartmentImages) {
-                if (extraImage.isEmpty()) continue;
 
-                String filename = StringUtils.cleanPath(extraImage.getOriginalFilename());
-                AmazonS3Util.uploadFile(uploadDir, filename, extraImage.getInputStream());
-
-                apartment.getImages().add(AmazonS3Constants.S3_BASE_URI + "/" + uploadDir + "/" + filename);
-            }
-        }
-    }
-
-    private Map<String, Object> generateMetadata(MultipartFile[] apartmentImages) throws IOException {
-        ChatClient chatClient = ChatClient.builder(chatModel).build();
-        ChatClient.ChatClientRequestSpec chatClientRequest = chatClient.prompt();
-        Resource image = new InputStreamResource(apartmentImages[0].getInputStream());
-        Message userMessage = new UserMessage("describe the image", List.of(new Media(MimeTypeUtils.parseMimeType(apartmentImages[0].getContentType()), image)));
-        Message systemMessage = new SystemMessage("describe the image");
-        chatClientRequest.messages(List.of(systemMessage, userMessage));
-        Map<String, Object> result = chatClientRequest.call().entity(new ParameterizedTypeReference<Map<String, Object>>() {});
-//        LOG.info("Successfully generated image metadata for content item with id {} and property {}: {}", request.id, request.property, result);
-        return result;
-    }
 
 }
--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/service/ApartmentService.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/service/ApartmentService.java
@@ -1,21 +1,53 @@
 package com.testehan.springai.immobiliare.service;
 
+import com.testehan.springai.immobiliare.constants.AmazonS3Constants;
 import com.testehan.springai.immobiliare.model.Apartment;
 import com.testehan.springai.immobiliare.model.PropertyType;
+import com.testehan.springai.immobiliare.model.auth.ImmobiliareUser;
 import com.testehan.springai.immobiliare.repository.ApartmentsRepository;
+import com.testehan.springai.immobiliare.security.UserService;
+import com.testehan.springai.immobiliare.util.AmazonS3Util;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.model.Media;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ApartmentService {
 
+    @Value("classpath:/prompts/system/PictureMetadataGeneration.txt")
+    private Resource systemPictureMetadataGeneration;
+    @Value("classpath:/prompts/user/PictureMetadataGeneration.txt")
+    private Resource userPictureMetadataGeneration;
+
     private final ApartmentsRepository apartmentsRepository;
     private final OpenAiService embedder;
+    private final ChatModel chatModel ;
+    private final UserService userService;
 
-    public ApartmentService(ApartmentsRepository apartmentsRepository, OpenAiService embedder) {
+    public ApartmentService(ApartmentsRepository apartmentsRepository, OpenAiService embedder, ChatModel chatModel, UserService userService) {
         this.apartmentsRepository = apartmentsRepository;
         this.embedder = embedder;
+        this.chatModel = chatModel;
+        this.userService = userService;
     }
 
     public List<Apartment> getApartmentsSemanticSearch(PropertyType propertyType, String city, Apartment apartment, String apartmentDescription) {
@@ -29,6 +61,58 @@ public class ApartmentService {
 
     public void saveApartment(Apartment apartment){
         apartmentsRepository.saveApartment(apartment);
+    }
+
+    public void saveApartmentAndImages(Apartment apartment, MultipartFile[] apartmentImages, ImmobiliareUser user) throws IOException {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateCustom = now.format(customFormatter);
+
+        apartment.setCreationDateTime(formattedDateCustom);
+        apartment.setLastUpdateDateTime(formattedDateCustom);
+        var apartmentInfoToEmbed = apartment.getApartmentInfoToEmbedd();
+
+        var mono = embedder.createEmbedding(apartmentInfoToEmbed);
+        List<Double> embeddings = mono.block();
+        System.out.println(embeddings.stream().map( d -> d.toString()).collect(Collectors.joining(" ")));
+        apartment.setPlot_embedding(embeddings);
+
+        saveApartment(apartment);
+        saveUploadedImages(apartment, apartmentImages);
+        generateMetadata(apartmentImages);
+        saveApartment(apartment);
+
+        user.setMaxNumberOfListedApartments(user.getMaxNumberOfListedApartments() - 1);
+        userService.updateUser(user);
+    }
+
+    private void saveUploadedImages(Apartment apartment, MultipartFile[] apartmentImages) throws IOException {
+        if (apartmentImages.length>0) {
+            var uploadDir = "apartment-images/" + apartment.getId();
+            for (MultipartFile extraImage : apartmentImages) {
+                if (extraImage.isEmpty()) continue;
+
+                String filename = StringUtils.cleanPath(extraImage.getOriginalFilename());
+                AmazonS3Util.uploadFile(uploadDir, filename, extraImage.getInputStream());
+
+                apartment.getImages().add(AmazonS3Constants.S3_BASE_URI + "/" + uploadDir + "/" + filename);
+            }
+        }
+    }
+
+    private Map<String, Object> generateMetadata(MultipartFile[] apartmentImages) throws IOException {
+        ChatClient chatClient = ChatClient.builder(chatModel).build();
+        ChatClient.ChatClientRequestSpec chatClientRequest = chatClient.prompt();
+        Resource image = new InputStreamResource(apartmentImages[0].getInputStream());
+        Message userMessage = new UserMessage(
+                userPictureMetadataGeneration.getContentAsString(Charset.defaultCharset()),
+                List.of(new Media(MimeTypeUtils.parseMimeType(apartmentImages[0].getContentType()), image))
+        );
+        Message systemMessage = new SystemMessage(systemPictureMetadataGeneration.getContentAsString(Charset.defaultCharset()));
+        chatClientRequest.messages(List.of(systemMessage, userMessage));
+        Map<String, Object> result = chatClientRequest.call().entity(new ParameterizedTypeReference<>() {});
+//        LOG.info("Successfully generated image metadata for content item with id {} and property {}: {}", request.id, request.property, result);
+        return result;
     }
 
 }
