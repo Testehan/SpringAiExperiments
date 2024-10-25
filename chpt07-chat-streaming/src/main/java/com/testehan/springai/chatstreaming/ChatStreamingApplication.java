2995bca6f7e3e944aeb18c7a0c4e13bcd8521915
DanTestehan <tdan89@yahoo.com>
2024-10-25 20:20:20 +0100

    chpt07-chat-streaming - chat streaming example

---
+++ chpt07-chat-streaming/src/main/java/com/testehan/springai/chatstreaming/ChatStreamingApplication.java
@@ -0,0 +1,14 @@
+package com.testehan.springai.chatstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ChatStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatStreamingApplication.class, args);
+	}
+
+}
