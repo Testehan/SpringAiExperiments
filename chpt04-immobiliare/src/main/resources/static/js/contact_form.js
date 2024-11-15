63f8880adeb9a35c3fbf99a4ce1e889ffd38951f
DanTestehan <tdan89@yahoo.com>
2024-11-15 20:20:20 +0100

    chpt04-immobiliare - use joi to validate contact form fields

--- chpt04-immobiliare/src/main/resources/static/js/contact_form.js
+++ chpt04-immobiliare/src/main/resources/static/js/contact_form.js
@@ -1,8 +1,32 @@
 emailjs.init('Nc9IU4enMFiqEX8QI');
 
 $(document).ready(function(){
+    const Joi = window.joi;
+    console.log(typeof Joi);
+
+    const schema = Joi.object({
+        user_name: Joi.string().required().label('Your Name'),
+        user_email: Joi.string().email({ tlds: { allow: false } }).required().label('Your Email'),
+        message: Joi.string().required().label('Message')
+    });
+
     $('#contactForm').on('submit', function(event) {
          event.preventDefault(); // Prevent form from refreshing the page
+
+        // Extract form data
+        const formData = {
+            user_name: $('#user_name').val(),
+            user_email: $('#user_email').val(),
+            message: $('#message').val(),
+        };
+
+        const { error } = schema.validate(formData, { abortEarly: true });
+        if (error) {
+            // Display validation errors
+            const errorMessages = error.details.map(err => err.message).join("\n");
+            alert("Validation Errors:\n" + errorMessages);
+            return;
+        }
 
         // Send form data using EmailJS
         emailjs.sendForm('service_07fsa7e', 'template_j12c142', this)
