1d98c13f5598ec497c05287d91fc728fb706d792
DanTestehan <tdan89@yahoo.com>
2025-01-10 20:20:20 +0100

    chpt04-immobiliare - custom confirmation dialog for deletion

--- chpt04-immobiliare/src/main/resources/static/js/property_form_images.js
+++ chpt04-immobiliare/src/main/resources/static/js/property_form_images.js
@@ -39,10 +39,16 @@ $(document).ready(function(){
     $("#addForm").submit(function(event) {
         handleSubmit(event);
     });
+// TODO Move this deletion logic to a new js file...
+    $('#deleteButton').on('click', function (event) {
+        event.preventDefault(); // Prevent default form submission
+        $('#deletion_confirmation').removeClass('hidden'); // Show the modal
+        const deleteUrl = $(this).data('url'); // Get URL from button data-url
+        $('#confirmDelete').data('url', deleteUrl); // Store URL in confirm button
+    });
 
-     $('#deleteButton').on('click', function () {
-        // Show confirmation dialog
-        if (confirm('Are you sure you want to delete this apartment?')) {
+    // When the user clicks the "Yes, Delete" button
+    $('#confirmDelete').on('click', function() {
         // Get the URL from the data attribute
         const url = $(this).data('url');
 
@@ -52,6 +58,7 @@ $(document).ready(function(){
           type: 'POST',
           success: function (response) {
             // Handle success (e.g., show a success message or reload the page)
+            $('#deletion_confirmation').addClass('hidden');
             Toastify({
                 text: response,
                 duration: 4000,
@@ -67,6 +74,7 @@ $(document).ready(function(){
           },
           error: function (xhr, status, error) {
             // Handle error (e.g., show an error message)
+            $('#deletion_confirmation').addClass('hidden');
             Toastify({
                text: xhr.responseText,
                duration: 3000,
@@ -79,7 +87,11 @@ $(document).ready(function(){
             }).showToast();
           }
         });
-        }
+    });
+
+    // When the user clicks the "Cancel" button
+    $('#cancelDelete').on('click', function() {
+        $('#deletion_confirmation').addClass('hidden'); // Hide the modal
     });
 
 });
