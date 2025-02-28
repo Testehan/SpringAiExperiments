8ca79552165ccd92c15a994dfee80b46ac7f427f
DanTestehan <tdan89@yahoo.com>
2025-02-28 20:20:20 +0100

    chpt04-immobiliare - separate schedueled job will send sms 1 day beore the listing will be deactivated

--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/configuration/ScheduledTasks.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/configuration/ScheduledTasks.java
@@ -10,6 +10,7 @@ import com.testehan.springai.immobiliare.util.LocaleUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -27,14 +28,16 @@ public class ScheduledTasks {
     private final ApartmentService apartmentService;
     private final EmailService emailService;
     private final SmsService smsService;
+    private final MessageSource messageSource;
     private final LocaleUtils localeUtils;
 
-    public ScheduledTasks(UserService userService, ApartmentService apartmentService, EmailService emailService, SmsService smsService,
+    public ScheduledTasks(UserService userService, ApartmentService apartmentService, EmailService emailService, SmsService smsService, MessageSource messageSource,
                           LocaleUtils localeUtils){
         this.userService = userService;
         this.apartmentService = apartmentService;
         this.emailService = emailService;
         this.smsService = smsService;
+        this.messageSource = messageSource;
         this.localeUtils = localeUtils;
     }
 
@@ -44,7 +47,6 @@ public class ScheduledTasks {
         log.info("Scheduled Task - The user available searches number was reset");
     }
     @Scheduled(cron = "0 0 3 * * ?")        // Code to run at 3 AM every day
-//    @Scheduled(cron = "0 0/3 * * * ?")          // runs every 3 mins for testing purposes
     public void deactivatedListingsLastUpdatedMoreThan2WeeksAgo() {
         LocalDateTime twoWeeksAgo = LocalDateTime.now().minus(14, ChronoUnit.DAYS);
         apartmentService.deactivateApartments(twoWeeksAgo);
@@ -52,7 +54,6 @@ public class ScheduledTasks {
     }
 
     @Scheduled(cron = "0 0 4 * * ?")        // Code to run at 4 AM every day
-//    @Scheduled(cron = "0 0/3 * * * ?")          // runs every 3 mins for testing purposes
     public void deleteDeletedUsersMoreThan1DayAgo() {
         LocalDateTime oneDayAgo = LocalDateTime.now().minus(1, ChronoUnit.DAYS);
         userService.deleteDeletedUsers(oneDayAgo);
@@ -62,10 +63,10 @@ public class ScheduledTasks {
 
     @Scheduled(cron = "0 0 8 * * ?")        // Code to run at 8 AM every day
 //    @Scheduled(cron = "0 0/3 * * * ?")          // runs every 3 mins for testing purposes
-    public void sendReactivationEmailOrSMS() {
+    public void sendReactivationEmail() {
 
         LocalDateTime twelveDaysAgo = LocalDateTime.now().minus(12, ChronoUnit.DAYS);
-        log.info("Scheduled Task - Reactivation emails or sms will be send to owners.");
+        log.info("Scheduled Task - Reactivation emails will be send to owners.");
 
         var listings = apartmentService.findByLastUpdateDateTimeBefore(twelveDaysAgo);
 
@@ -77,15 +78,33 @@ public class ScheduledTasks {
             if (ContactValidator.isValidEmail(contact)){
                 emailService.sendReactivateListingEmail(contact,"",listing.getName(),reactivateLink, localeUtils.getCurrentLocale());
             }
+            
+        }
 
-            // TODO Think about when sending an sms is really needed..
+    }
+
+    @Scheduled(cron = "0 0 8 * * ?")        // Code to run at 8 AM every day
+//    @Scheduled(cron = "0 0/3 * * * ?")          // runs every 3 mins for testing purposes
+    public void sendReactivationSMS() {
+
+        LocalDateTime thirteenDaysAgo = LocalDateTime.now().minus(13, ChronoUnit.DAYS);
+        log.info("Scheduled Task - Reactivation sms will be send to owners.");
+
+        var listings = apartmentService.findByLastUpdateDateTimeBefore(thirteenDaysAgo);
+
+        for (Apartment listing : listings){
+            log.info(listing.getLastUpdateDateTime() + "       " + listing.getName());
+            var contact = listing.getContact();
+            var reactivateLink = appUrl + "/reactivate?token="+listing.getActivationToken()+"&id=" + listing.getId().toString();
+
             if (ContactValidator.isValidPhoneNumber(contact,"RO")){
                 var phoneWithPrefix = ContactValidator.getPhoneNumberWithPrefix(contact, "RO");
                 if (phoneWithPrefix.isEmpty()){
-                    log.error("Can't send SMS for + " + contact + " of listing " + listing.getName());
+                    log.error("Can't send SMS for {} of listing named {}", contact , listing.getName());
                 } else {
-                    var smsMessage = "Hi! Reactivate listing: " + reactivateLink;
-                    smsService.sendSms(phoneWithPrefix.get(), smsMessage);    // "or reply yes" => this is not implemented yet, see notes from SmsService
+                    var smsMessage = messageSource.getMessage("sms.keep.active", null,localeUtils.getCurrentLocale()) + " " + reactivateLink;
+// TODO when you will use your domain, you can test this out, as right now the sms length exceeds the allowed number of chars, because of the currently long domain name..
+                    //                    smsService.sendSms(phoneWithPrefix.get(), smsMessage);    // "or reply yes" => this is not implemented yet, see notes from SmsService
                 }
 
             }
