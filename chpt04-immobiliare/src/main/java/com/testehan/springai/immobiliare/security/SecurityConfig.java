e6b9a3b1f55a953a82057c8ae455ab076cbf2870
DanTestehan <tdan89@yahoo.com>
2025-04-08 20:20:20 +0100

    chpt04-immobiliare - monitoring with actuator prometheus and grafana part 2

--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/security/SecurityConfig.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
@@ -16,6 +17,7 @@ import org.springframework.security.web.authentication.RememberMeServices;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
+@Order(10) // Lower numbers have higher precedence
 public class SecurityConfig {
 
     @Value("${rememberme.token.key}")
@@ -50,42 +52,44 @@ public class SecurityConfig {
         LoggingAuthorizationRequestResolver customResolver = new LoggingAuthorizationRequestResolver(defaultResolver);
 
         http.csrf(CsrfConfigurer::disable);
-        http.authorizeHttpRequests(req -> req
-                .requestMatchers("/","/help","/blog","/contact", "/error**", "/error-login", "/login-modal",
-                        "/reactivate","/confirmation","/accept-gdpr","/privacy-policy","/terms", "/s/**",
-                        "/actuator/prometheus").permitAll()
-                .requestMatchers(antMatcher("/css/**")).permitAll()
-                .requestMatchers(antMatcher("/js/**")).permitAll()
-                .requestMatchers(antMatcher("/webjars/**")).permitAll()
-                .requestMatchers(antMatcher("/images/**")).permitAll()
+
+        http
+        .authorizeHttpRequests(req -> req
+            .requestMatchers("/","/help","/blog","/contact", "/error**", "/error-login", "/login-modal",
+                    "/reactivate","/confirmation","/accept-gdpr","/privacy-policy","/terms", "/s/**").permitAll()
+            .requestMatchers(antMatcher("/css/**")).permitAll()
+            .requestMatchers(antMatcher("/js/**")).permitAll()
+            .requestMatchers(antMatcher("/webjars/**")).permitAll()
+            .requestMatchers(antMatcher("/images/**")).permitAll()
 //                .requestMatchers(SecuredPaths.SECURED_URLS.toArray(new String[0])).authenticated()
 
-                                // if this works, we don't need the above SECURED_URLs stuff because all other enpoints,
-                                // except the ones permitted above, must be authenticated
-                 .anyRequest().authenticated()
-                )
-            .oauth2Login( oauth2Login -> oauth2Login
-                .loginPage("/login-modal")
-                .permitAll()
-                .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
-                        .userService(customerOAuth2UserService))
-                .successHandler(oAuth2LoginSuccessHandler())
-                .authorizationEndpoint(authEndpoint -> authEndpoint.authorizationRequestResolver(customResolver))
-            )
+                            // if this works, we don't need the above SECURED_URLs stuff because all other enpoints,
+                            // except the ones permitted above, must be authenticated
 
-                .rememberMe(httpSecurityRememberMeConfigurer -> {
-                    httpSecurityRememberMeConfigurer.tokenValiditySeconds(1209600)  // Set token validity to 14 days (in seconds))
-                            .key(rememberMeKey)
-                            .rememberMeServices(autoRememberMeServices())
-                            .useSecureCookie(false);
-                })
-            .logout((logout) -> logout.logoutSuccessUrl("/")        // after logout redirect user to /index
-                                    .invalidateHttpSession(true)
-                                    .clearAuthentication(true)
-                                    .deleteCookies("JSESSIONID"))
-            .exceptionHandling(ex -> {
-                ex.authenticationEntryPoint(customAuthenticationEntryPoint);
-            });
+            .anyRequest().authenticated()
+        )
+        .oauth2Login( oauth2Login -> oauth2Login
+            .loginPage("/login-modal")
+            .permitAll()
+            .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+                    .userService(customerOAuth2UserService))
+            .successHandler(oAuth2LoginSuccessHandler())
+            .authorizationEndpoint(authEndpoint -> authEndpoint.authorizationRequestResolver(customResolver))
+        )
+
+        .rememberMe(httpSecurityRememberMeConfigurer -> {
+            httpSecurityRememberMeConfigurer.tokenValiditySeconds(1209600)  // Set token validity to 14 days (in seconds))
+                    .key(rememberMeKey)
+                    .rememberMeServices(autoRememberMeServices())
+                    .useSecureCookie(false);
+        })
+        .logout((logout) -> logout.logoutSuccessUrl("/")        // after logout redirect user to /index
+                                .invalidateHttpSession(true)
+                                .clearAuthentication(true)
+                                .deleteCookies("JSESSIONID"))
+        .exceptionHandling(ex -> {
+            ex.authenticationEntryPoint(customAuthenticationEntryPoint);
+        });
 
         return http.build();
     }
