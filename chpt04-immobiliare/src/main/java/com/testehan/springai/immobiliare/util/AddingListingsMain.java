d220669cb0d9a7e61cc917f089c421f924e4dc97
DanTestehan <tdan89@yahoo.com>
2025-04-14 20:20:20 +0100

    chpt04-immobiliare - improved the automatic posting code

--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/util/AddingListingsMain.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/util/AddingListingsMain.java
@@ -7,6 +7,7 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.ByteArrayOutputStream;
@@ -14,6 +15,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 class ListingImage {
@@ -69,67 +72,77 @@ public class AddingListingsMain {
 
             // Create a RestTemplate instance
             RestTemplate restTemplate = new RestTemplate();
-            String endpoint = "https://casa-mia.fly.dev/api/apartments/save";
-            String sessionId = "02D2653DA073350165B707E0843DA916";
+            String endpoint = "https://casa-mia.fly.dev/api/apartments/batchsave";
+            String sessionId = "F914E940611214276E455582AC1373BF";
 
             Root root = objectMapper.readValue(new File("/Users/danteshte/JavaProjects/spring-ai-experiments/chpt04-immobiliare/src/test/resources/data2.json"), Root.class);
             for (Listing listing : root.data) {
-                // Prepare the MultipartFile[] for images (if any)
-                MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+                try {
+                    // Prepare the MultipartFile[] for images (if any)
+                    MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
 
-                body.add("name", listing.fullTitle);
-                body.add("city", "Cluj-Napoca");
-                body.add("area", listing.address);
-                body.add("shortDescription", listing.fullDescription);
-                body.add("price", Integer.valueOf(listing.price));
-                body.add("propertyType", "rent");
-                body.add("surface", Integer.valueOf(listing.surface));
-                body.add("noOfRooms", Integer.valueOf(listing.noOfRooms));
-                body.add("floor", listing.floor);
-                body.add("contact", listing.phone);
-                body.add("ownerName", listing.ownerName);
-                body.add("active", false);
+                    body.add("name", listing.fullTitle);
+                    body.add("city", "Cluj-Napoca");
+                    body.add("area", listing.address);
+                    body.add("shortDescription", listing.fullDescription);
+                    body.add("price", Integer.valueOf(listing.price));
+                    body.add("propertyType", "rent");
+                    body.add("surface", Integer.valueOf(listing.surface));
+                    body.add("noOfRooms", Integer.valueOf(listing.noOfRooms));
+                    body.add("floor", listing.floor);
+                    body.add("contact", listing.phone);
+                    body.add("ownerName", listing.ownerName);
+                    body.add("active", false);
+                    body.add("availableFrom", getAvailableFromToday());
 
 
-                if (listing.listingImages != null) {
-                    int i = 1;
-                    for (ListingImage img : listing.listingImages) {
-                        // Download image from the URL
-                        URL url = new URL(img.imageUrl); // The image URL (online)
-                        InputStream inputStream = url.openStream();
-                        byte[] imageBytes = toByteArray(inputStream); // Convert InputStream to byte array
+                    if (listing.listingImages != null) {
+                        int i = 1;
+                        for (ListingImage img : listing.listingImages) {
+                            try {
+                                // Download image from the URL
+                                URL url = new URL(img.imageUrl); // The image URL (online)
+                                InputStream inputStream = url.openStream();
+                                byte[] imageBytes = toByteArray(inputStream); // Convert InputStream to byte array
 
-                        final int j = i;
-                        ByteArrayResource fileResource = new ByteArrayResource(imageBytes) {
-                            @Override
-                            public String getFilename() {
-                                return "image" + j + ".jpg"; // or whatever file name and extension you want
+                                final int j = i;
+                                ByteArrayResource fileResource = new ByteArrayResource(imageBytes) {
+                                    @Override
+                                    public String getFilename() {
+                                        return "image" + j + ".jpg"; // or whatever file name and extension you want
+                                    }
+                                };
+                                i++;
+
+                                // Add to the body
+                                body.add("apartmentImages", fileResource);
+                            } catch (Exception e) {
+                                System.out.println("Error processing image " + img.imageUrl);
+                                System.out.println(e.getMessage());
                             }
-                        };
-                        i++;
-
-                        // Add to the body
-                        body.add("apartmentImages", fileResource);
+                        }
                     }
+
+                    // Prepare the request
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+                    headers.set("Cookie", "JSESSIONID=" + sessionId);
+                    HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+                    // Make the POST request
+                    ResponseEntity<String> response = restTemplate.exchange(endpoint, HttpMethod.POST, requestEntity, String.class);
+
+                    // Handle the response
+                    if (response.getStatusCode() == HttpStatus.OK) {
+                        System.out.println("Apartment successfully saved!");
+                    } else {
+                        System.out.println("Failed to save apartment. Status code: " + response.getStatusCode() + " body: " + response.getBody());
+                    }
+
+                    Thread.sleep(40000);
+                } catch (HttpClientErrorException e) {
+                    System.out.println(e.getMessage());
                 }
-
-                // Prepare the request
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-                headers.set("Cookie", "JSESSIONID=" + sessionId);
-                HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-
-                // Make the POST request
-                ResponseEntity<String> response = restTemplate.exchange(endpoint, HttpMethod.POST, requestEntity, String.class);
-
-                // Handle the response
-                if (response.getStatusCode() == HttpStatus.OK) {
-                    System.out.println("Apartment successfully saved!");
-                } else {
-                    System.out.println("Failed to save apartment. Status code: " + response.getStatusCode());
-                }
-
-                Thread.sleep(20000);
 
 
                 System.out.println("-----------------------------");
@@ -150,5 +163,12 @@ public class AddingListingsMain {
             byteArrayOutputStream.write(bytes, 0, read);
         }
         return byteArrayOutputStream.toByteArray();
+    }
+
+    private static String getAvailableFromToday(){
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        return today.format(formatter);
     }
 }
