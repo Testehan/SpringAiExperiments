f93bc784acbce2ec9a36aef8520d2654031a8790
DanTestehan <tdan89@yahoo.com>
2025-05-04 20:20:20 +0100

    chpt04-immobiliare - added sorting to contact attemps

--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/controller/AdminViewController.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/controller/AdminViewController.java
@@ -1,13 +1,13 @@
 package com.testehan.springai.immobiliare.controller;
 
-import com.testehan.springai.immobiliare.repository.ContactAttemptRepository;
 import com.testehan.springai.immobiliare.model.ContactAttempt;
+import com.testehan.springai.immobiliare.repository.ContactAttemptRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/admin")
@@ -26,9 +26,24 @@ public class AdminViewController {
 
 
     @GetMapping("/contact-attempts")
-    public String getContactAttempts(Model model) {
+    public String getContactAttempts(Model model,
+                                     @RequestParam(defaultValue = "createdAt") String sortBy,
+                                     @RequestParam(defaultValue = "desc") String direction,
+                                     @RequestParam(defaultValue = "0") int page) {
+
+        Sort sort = direction.equalsIgnoreCase("desc") ? Sort.by(Sort.Order.desc(sortBy)) : Sort.by(Sort.Order.asc(sortBy));
+
+        // PageRequest with page number and sorting
+        PageRequest pageRequest = PageRequest.of(page, 10, sort);
+        Page<ContactAttempt> attempts = contactAttemptRepository.findAll(pageRequest);
+
         model.addAttribute("newContactAttempt", new ContactAttempt());
-        model.addAttribute("contactAttempts", contactAttemptRepository.findAll());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", attempts.getTotalPages());
+        model.addAttribute("contactAttempts", attempts.getContent());
+        model.addAttribute("currentSort", sortBy);
+        model.addAttribute("currentDirection", direction);
+
         return "admin-contact-attempts";
     }
 
