461e57a73d8b65676e70a911c3d736728a1cb86b
DanTestehan <tdan89@yahoo.com>
2025-05-06 20:20:20 +0100

    chpt04-immobiliare - use the www url + issue with latest spring ai mongo code that made declare the bean from spring ai myself

--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/configuration/MongoDBConfig.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/configuration/MongoDBConfig.java
@@ -10,9 +10,16 @@ import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.util.MimeType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class MongoDBConfig {
@@ -24,6 +31,16 @@ public class MongoDBConfig {
 
     @Value("${spring.data.mongodb.database}")
     private String MONGO_DB_NAME;
+
+    private final Converter<MimeType, String> libraryMimeTypeToStringConverter;
+    private final Converter<String, MimeType> libraryStringToMimeTypeConverter;
+
+    public MongoDBConfig(
+            @Qualifier("mimeTypeToStringConverter") Converter<MimeType, String> mimeTypeToStringConverter,
+            @Qualifier("stringToMimeTypeConverter") Converter<String, MimeType> stringToMimeTypeConverter) {
+        this.libraryMimeTypeToStringConverter = mimeTypeToStringConverter;
+        this.libraryStringToMimeTypeConverter = stringToMimeTypeConverter;
+    }
 
     @Bean
     public MongoClient mongoClient() {
@@ -48,5 +65,16 @@ public class MongoDBConfig {
         LOGGER.info("Using {}", MONGO_DB_NAME);
         return mongoClient.getDatabase(MONGO_DB_NAME);
     }
+
+    @Bean
+    public MongoCustomConversions mongoCustomConversions() {
+        List<Converter<?, ?>> allConverters = new ArrayList<>();
+
+        allConverters.add(libraryMimeTypeToStringConverter);
+        allConverters.add(libraryStringToMimeTypeConverter);
+
+        return new MongoCustomConversions(allConverters);
+    }
+
 
 }
