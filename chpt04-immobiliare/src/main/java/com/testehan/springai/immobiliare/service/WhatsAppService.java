7db6ad96cb1bdf121ef980c285d0caaf5f750d8a
DanTestehan <tdan89@yahoo.com>
2025-05-08 20:20:20 +0100

    chpt04-immobiliare - whatsapp service mark as read method - this marks all messages from the conversation as read, even audio, images which i ignore

--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/service/WhatsAppService.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/service/WhatsAppService.java
@@ -1,5 +1,7 @@
 package com.testehan.springai.immobiliare.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -8,8 +10,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class WhatsAppService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WhatsAppService.class);
+
+    public static final String WHATSAPP_API_22_MESSAGES = "https://graph.facebook.com/v22.0/%s/messages";
 
     @Value("${whatsapp.api.access.token}")
     private String WHATS_APP_ACCESS_TOKEN;
@@ -18,7 +27,7 @@ public class WhatsAppService {
     private String PHONE_NUMBER_ID;
 
     public void sendMessage(String to, String messageText) {
-        String url = String.format("https://graph.facebook.com/v22.0/%s/messages", PHONE_NUMBER_ID);
+        String url = String.format(WHATSAPP_API_22_MESSAGES, PHONE_NUMBER_ID);
 
         RestTemplate restTemplate = new RestTemplate();
 
@@ -46,5 +55,29 @@ public class WhatsAppService {
 
         System.out.println("Response: " + response.getBody());
     }
+
+    public void markMessageAsRead(String messageId, String phoneNumberId) {
+        String url = String.format(WHATSAPP_API_22_MESSAGES, phoneNumberId);
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(WHATS_APP_ACCESS_TOKEN);
+
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("messaging_product", "whatsapp");
+        payload.put("status", "read");
+        payload.put("message_id", messageId);
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(payload, headers);
+
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+            LOGGER.info("Marked message {} as read. Response: {}", messageId, response.getBody());
+        } catch (Exception e) {
+            LOGGER.error("Failed to mark message {} as read. {}",messageId, e.getMessage());
+        }
+    }
+
 
 }
