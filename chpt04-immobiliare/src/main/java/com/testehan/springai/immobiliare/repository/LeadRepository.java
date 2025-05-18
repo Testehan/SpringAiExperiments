ad3af7ed65e01bf3efea3886fe6a6f2a9bbc43e5
DanTestehan <tdan89@yahoo.com>
2025-05-18 20:20:20 +0100

    chpt04-immobiliare - search lead text method

--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/repository/LeadRepository.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/repository/LeadRepository.java
@@ -2,7 +2,10 @@ package com.testehan.springai.immobiliare.repository;
 
 import com.testehan.springai.immobiliare.model.Lead;
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +20,10 @@ public interface LeadRepository extends MongoRepository<Lead, ObjectId> {
     List<Lead> findByListingUrlContaining(String filterUrlValue);
 
     List<Lead> findByListingUrlContainingAndStatus(String filterUrlValue, String statusValue);
+
+    @Query("{ '$or': [ " +
+            " { 'phoneNumber': { $regex: ?0, $options: 'i' } }, " +
+            " { 'listingUrl':  { $regex: ?0, $options: 'i' } } " +
+            "] }")
+    Page<Lead> searchLeads(String search, Pageable pageable);
 }
