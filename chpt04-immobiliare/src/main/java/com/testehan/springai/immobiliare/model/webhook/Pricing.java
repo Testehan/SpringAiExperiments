8916bd1e991a2e60463b9379d9fe31b2b50ceebe
DanTestehan <tdan89@yahoo.com>
2025-05-27 20:20:20 +0100

    chpt04-immobiliare - FIXED some whatsapp error that was appearing when a new message was recieved

--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/model/webhook/Pricing.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/model/webhook/Pricing.java
@@ -1,8 +1,10 @@
 package com.testehan.springai.immobiliare.model.webhook;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Pricing {
     private boolean billable;
     private String pricing_model;
