fa2ce0c157f4cb75d54e2ae6500e2647d811c7f5
DanTestehan <tdan89@yahoo.com>
2025-06-08 20:20:20 +0100

    chpt04-immobiliare - disable the whatsapp api refresh token

--- chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/service/WhatsAppTokenService.java
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/service/WhatsAppTokenService.java
@@ -4,13 +4,8 @@ import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.Map;
 
 
 // since i don't have a verified meta business, i have to use short duration 1 hour tokens
@@ -47,29 +42,30 @@ public class WhatsAppTokenService {
         return currentToken;
     }
 
-    @Scheduled(fixedDelay = 50 * 60 * 1000) // refresh every 50 min
+    // todo for now i use another service
+//    @Scheduled(fixedDelay = 50 * 60 * 1000) // refresh every 50 min
     public void scheduledRefresh() {
         refreshToken();
     }
 
     private void refreshToken() {
-        try {
-            String url = UriComponentsBuilder.fromHttpUrl(tokenUrl)
-                    .queryParam("grant_type", "fb_exchange_token")
-                    .queryParam("client_id", appId)
-                    .queryParam("client_secret", appSecret)
-                    .queryParam("fb_exchange_token", currentToken)
-                    .toUriString();
-
-            ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
-            if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-                currentToken = (String) response.getBody().get("access_token");
-                LOGGER.info("Refreshed WhatsApp token: {}", currentToken);
-            } else {
-                LOGGER.error("Failed to refresh token: {}", response.getStatusCode());
-            }
-        } catch (Exception e) {
-            LOGGER.error("Exception refreshing WhatsApp token: {}" , e.getMessage());
-        }
+//        try {
+//            String url = UriComponentsBuilder.fromHttpUrl(tokenUrl)
+//                    .queryParam("grant_type", "fb_exchange_token")
+//                    .queryParam("client_id", appId)
+//                    .queryParam("client_secret", appSecret)
+//                    .queryParam("fb_exchange_token", currentToken)
+//                    .toUriString();
+//
+//            ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
+//            if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
+//                currentToken = (String) response.getBody().get("access_token");
+//                LOGGER.info("Refreshed WhatsApp token: {}", currentToken);
+//            } else {
+//                LOGGER.error("Failed to refresh token: {}", response.getStatusCode());
+//            }
+//        } catch (Exception e) {
+//            LOGGER.error("Exception refreshing WhatsApp token: {}" , e.getMessage());
+//        }
     }
 }
