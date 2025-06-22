fcaf161b501697412fcfb41e2788f0e91157b9bd
DanTestehan <tdan89@yahoo.com>
2025-06-22 20:20:20 +0100

    chpt04-immobiliare - new collection for holding various counters for other collections

---
+++ chpt04-immobiliare/src/main/java/com/testehan/springai/immobiliare/model/DatabaseSequence.java
@@ -0,0 +1,20 @@
+package com.testehan.springai.immobiliare.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Document(collection = "sequences")
+public class DatabaseSequence {
+    @Id
+    private String id;
+    private long seq;
+
+}
